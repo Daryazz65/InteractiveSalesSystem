0160b6569ebef92fb9348cb5755ee3686b86cdbb
Дарья <@HOME-PC>
2026-05-07 11:44:07 +0300

    Write a class to select a parser

--- src/main/java/ru/cementpromo/parser/ParserFactory.java
+++ src/main/java/ru/cementpromo/parser/ParserFactory.java
@@ -1,4 +1,15 @@
 package ru.cementpromo.parser;
 
+import java.nio.file.Path;
+
 public class ParserFactory {
+    public static OrderParser select(Path file) {
+        String fileName = file.getFileName().toString().toLowerCase();
+
+        if(fileName.endsWith(".txt")) {
+            return new PipeSeparatorParser();
+        }
+
+        return new HashSeparatorParser();
+    }
 }
